98824f1669bbd5183428e04dcfd02baeef8771d6
Abhishekanand32 <anandbarhiya2003@gmail.com>
2024-08-26 09:42:38 +0530

    Update DeganToken.sol

--- DeganToken.sol
+++ DeganToken.sol
@@ -6,43 +6,63 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract DeganToken is ERC20 {
     address public owner;
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    struct NFT {
+        string name;
+        string url;
+        bool isAvailable;
+        uint256 price;
+    }
 
-    constructor() ERC20("Degan", "DGN")  {
+    mapping(string => NFT) public NFTs;
+    string[] public allNFTs;
+
+    // Mapping to store NFTs bought by each user
+    mapping(address => string[]) public userNFTs;
+
+    // Events
+    event Minted(address indexed to, uint256 amount);
+    event Burned(address indexed from, uint256 amount);
+    event NFTGenerated(string name, string url, uint256 price);
+    event Redeemed(address indexed from, string name);
+
+    constructor() ERC20("DeganToken", "DGN") {
         owner = msg.sender;
-        emit OwnershipTransferred(address(0), msg.sender);
     }
 
     modifier onlyOwner() {
-        require(owner == msg.sender, "MyToken: caller is not the owner");
+        require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    function transferOwnership(address newOwner) public onlyOwner {
-        require(newOwner != address(0), "MyToken: new owner is the zero address");
-        emit OwnershipTransferred(owner, newOwner);
-        owner = newOwner;
-    }
-
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+        emit Minted(to, amount);
     }
 
-    function burn(uint256 amount) public {
+    function burn(uint256 amount) external onlyOwner {
         _burn(msg.sender, amount);
+        emit Burned(msg.sender, amount);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
-        require(_validRecipient(recipient), "MyToken: invalid recipient");
-        return super.transfer(recipient, amount);
+    function generateNFT(string memory name, string memory url, uint256 price) external onlyOwner {
+        NFTs[name] = NFT({name: name, url: url, price: price, isAvailable: true});
+        allNFTs.push(name);
+        emit NFTGenerated(name, url, price);
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
-        require(_validRecipient(recipient), "MyToken: invalid recipient");
-        return super.transferFrom(sender, recipient, amount);
+    function redeem(string memory name) external {
+        require(balanceOf(msg.sender) >= NFTs[name].price, "Insufficient payment");
+        _burn(msg.sender, NFTs[name].price);
+        NFTs[name].isAvailable = false;
+        userNFTs[msg.sender].push(name);
+        emit Redeemed(msg.sender, name);
     }
 
-    function _validRecipient(address to) private pure returns (bool) {
-        return to != address(0);
+    function getAllNFTs() external view returns (string[] memory) {
+        return allNFTs;
+    }
+
+    function getUserNFTs() external view returns (string[] memory) {
+        return userNFTs[msg.sender];
     }
 }
